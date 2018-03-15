cd6c9bf3d45d52001e18d769eefc8bd011186ce0
Sommai Krangpanich <sommai.k@gmail.com>
2018-03-15 10:34:04 +0700

    add new function

---
+++ src/servlet/spring/Kyle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet.spring;
+
+/**
+ *
+ * @author sommaik
+ */
+public class Kyle {
+    
+//    Kyle(){}
+//  
+    String name;
+    
+    Kyle(String name){
+        this.name = name;
+        this.hello();
+    }
+    
+    public String sayHello(){
+        return "Hello "+this.name;
+    }
+    
+    public void hello(){
+        System.out.println(
+                "Local say hello "+this.name
+        );
+    }
+    
+}
--- src/servlet/spring/ServletSpring.java
+++ src/servlet/spring/ServletSpring.java
@@ -16,7 +16,8 @@ public class ServletSpring {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World from git.");
+        Kyle k = new Kyle("John");
+        System.out.println(k.sayHello());
     }
     
 }
